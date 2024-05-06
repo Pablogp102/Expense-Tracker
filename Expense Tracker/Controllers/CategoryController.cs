b840bf706e84f63d1e7ff7a2756e903370eb7ef1
Pablogp102 <pablopg102@gmail.com>
2024-05-06 16:48:42 +0200

    Category Property add TitleWithIcon, remove unnecessary Views, add css style for buttons,grid, add paging

--- Expense Tracker/Controllers/CategoryController.cs
+++ Expense Tracker/Controllers/CategoryController.cs
@@ -26,24 +26,6 @@ namespace Expense_Tracker.Controllers
                         Problem("Entity set 'ApplicationDbContext.Categories' is null");
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(c => c.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // GET: Category/AddOrEdit
         public IActionResult AddOrEdit(int id = 0)
         {
@@ -74,30 +56,15 @@ namespace Expense_Tracker.Controllers
         }
 
 
-
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
+            if(_context.Categories == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Categories' is null");
+            }
             var category = await _context.Categories.FindAsync(id);
             if (category != null)
             {
@@ -108,9 +75,6 @@ namespace Expense_Tracker.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoryExists(int id)
-        {
-            return _context.Categories.Any(e => e.CategoryId == id);
-        }
+
     }
 }
--- Expense Tracker/Models/Category.cs
+++ Expense Tracker/Models/Category.cs
@@ -6,9 +6,16 @@ namespace Expense_Tracker.Models
     public class Category
     {
         public int CategoryId { get; set; }
-        public string Title { get; set; }
+        public string Title { get; set; } = default!;
         public string Icon { get; set; } = "";
         public string Type { get; set; } = "Expense";
-
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
     }
 }
